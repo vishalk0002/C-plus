4701c5c4b5f4ca257e0b25d7951f120686fc8029
Vishal Shrivastava <134785450+vishalk0002@users.noreply.github.com>
2024-01-23 22:41:25 +0530

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n;
+     cout<<" Enter the number that you are want to print ";
+     cin>>n;
+     cout<<"\n";
+     cout<<" the number is : "<<n;
+     return 0;
+}
